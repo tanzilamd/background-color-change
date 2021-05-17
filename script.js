dedeccd3fa57deb83bdc0851eacb7cb8b016f1d2
tanzilamd <mdtanzil858@gmail.com>
2021-05-17 21:54:29 +0600

    full project completed

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1st Button
+function handleClick1(){
+    document.body.style.backgroundColor = 'orange';
+}
+const firstButton = document.getElementById('first');
+firstButton.onclick = handleClick1;
+// 2nd Button 
+function handleClick(){
+    document.body.style.backgroundColor = 'tomato';
+}
+var seceondButton = document.getElementById('second');
+seceondButton.onclick =  handleClick;
+// 3rd Button
+function handleClick3(){
+    document.body.style.backgroundColor = 'lightblue';
+}
+const thirdButton = document.getElementById('third');
+thirdButton.onclick = handleClick3;
+// 4th Button
+function handleClick4(){
+    document.body.style.backgroundColor = 'lightyellow';
+}
+const fourthButton = document.getElementById('fourth');
+fourthButton.onclick = handleClick4;
